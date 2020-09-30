0b0ac79bd357755b4e1322887594e76a3b3b4799
eqbrownISF <eqbrown@isf.edu.hk>
2020-09-30 13:08:16 +0800

    uncomment setup1

--- setup1.sh
+++ setup1.sh
@@ -5,7 +5,7 @@
 #############
 
 echo "--Updating Raspbery Pi--"
-#sudo apt update
+sudo apt update
 sudo apt full-upgrade
 
 echo "--python3 version--"
@@ -14,20 +14,20 @@ python3 -version
 #############
 #Install and Updates pip
 #############
-# echo "--Installing and updating pip--"
+echo "--Installing and updating pip--"
 
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# sudo python get-pip.py
-# pip install -U pip
-# echo "--pip Version--"
-# pip --version
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+pip install -U pip
+echo "--pip Version--"
+pip --version
 
 # #############
 # #Install rmate for remote atom
 # #############
-# echo "--Installing rmate for remote atom--"
+echo "--Installing rmate for remote atom--"
 
-# sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
-# sudo chmod +x /usr/local/bin/rmate
-# sudo mv /usr/local/bin/rmate /usr/local/bin/ratom
+sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
+sudo chmod +x /usr/local/bin/rmate
+sudo mv /usr/local/bin/rmate /usr/local/bin/ratom
 
