1082f966339424bbf23aeb571989675287abf9cc
eqbrownISF <eqbrown@isf.edu.hk>
2020-09-30 12:47:21 +0800

    add more setup files

---
+++ setup2a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#############
+#Download Python3.8
+#############
+
+echo "--Downloading Python3.8 files--"
+
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev tar wget vim
+#wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+#sudo tar xf Python-3.8.0.tgz
+
---
+++ setup2b.sh
@@ -0,0 +1,10 @@
+echo "--Installing Python3.8 files--"
+cd Python-3.8.0
+sudo ./configure --enable-optimizations
+sudo make -j 4
+sudo make altinstall
+echo "--Python3.8 Version--"
+python3.8 --version
+alias python=python3.8
+echo "--Python Version--"
+python --version
