61608eced0c0653a0dedebcab4b1237576004fa9
eqbrownISF <eqbrown@isf.edu.hk>
2020-09-30 12:32:06 +0800

    setup1 upload

---
+++ setup1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#############
+#Update Raspberry Pi
+#############
+
+echo "--Updating Raspbery Pi--"
+#sudo apt update
+sudo apt full-upgrade
+
+echo "--python3 version--"
+python3 -version
+
+#############
+#Install and Updates pip
+#############
+# echo "--Installing and updating pip--"
+
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# sudo python get-pip.py
+# pip install -U pip
+# echo "--pip Version--"
+# pip --version
+
+# #############
+# #Install rmate for remote atom
+# #############
+# echo "--Installing rmate for remote atom--"
+
+# sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
+# sudo chmod +x /usr/local/bin/rmate
+# sudo mv /usr/local/bin/rmate /usr/local/bin/ratom
+
