ecc3b964a9b679d44bbc432bf176aa7d7019832c
Jacob <jacob.h.wolf@gmail.com>
2020-10-05 08:15:34 +0100

    Add batadv-vis server starting to mesh-network setup

--- mesh-network/start-batman-adv.sh
+++ mesh-network/start-batman-adv.sh
@@ -9,3 +9,9 @@ sudo batctl gw_mode client
 # Activates batman-adv interface
 sudo ifconfig wlan0 up
 sudo ifconfig bat0 up
+
+#set bat0 mac address as wlan0 mac address plus one
+sudo ip link set bat0 address b8:27:eb:8d:b4:1a
+
+# set IPv6 link-local address
+sudo ip -6 addr add fe80::ba27:ebff:fe8d:b41a/64 dev bat0
