b9e501db966d71fb3d7d7a360cd6871e4de9ed16
eqbrownISF <eqbrown@isf.edu.hk>
2020-10-05 11:32:00 +0800

    edit chmod

--- setup3.sh
+++ setup3.sh
@@ -12,7 +12,7 @@ echo "-- Setting up batman-adv mesh networking --"
 mkdir -p ~/bin
 sudo curl -o ~/bin/wlan0.mesh https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/wlan0.mesh
 sudo curl -o ~/bin/setup-mesh-network.sh https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/setup-mesh-network.sh
-sudo chmod +x setup-mesh-network.sh
+sudo chmod +x ~/bin/setup-mesh-network.sh
 sudo curl -o ~/bin/start-batman-adv.sh https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/start-batman-adv.sh
 sudo chmod +x ~/bin/start-batman.sh
 sudo curl -o /etc/bat-hosts https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/bat-hosts
