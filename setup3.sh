f5dd9e2555ea36e5ff3b3aec381df7b87194f698
Jacob Wolf <jacob.h.wolf@gmail.com>
2020-10-05 11:04:53 +0800

    Making downloaded scripts executable

--- setup3.sh
+++ setup3.sh
@@ -12,13 +12,15 @@ echo "-- Setting up batman-adv mesh networking --"
 mkdir -p ~/bin
 sudo curl -o ~/bin/wlan0.mesh https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/wlan0.mesh
 sudo curl -o ~/bin/setup-mesh-network.sh https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/setup-mesh-network.sh
+sudo chmod +x setup-mesh-network.sh
 sudo curl -o ~/bin/start-batman-adv.sh https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/start-batman-adv.sh
-chmod +x start-batman.sh
+sudo chmod +x ~/bin/start-batman.sh
 sudo curl -o /etc/bat-hosts https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/bat-hosts
 
 echo "-- Setting up hotspot switching service --"
 sudo curl -o /etc/systemd/system/setup-hotspot-switching.service https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/setup-hotspot-switching.service
 sudo curl -o /usr/bin/setup-hotspot-switching https://raw.githubusercontent.com/the-isf-academy/cs10_raspberrypiSetup/master/mesh-network/setup-hotspot-switching
+sudo chmod +x /usr/bin/setup-hotspot-switching
 sudo systemctl enable setup-hotspot-switching.service
 
 
